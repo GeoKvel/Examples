79627997290e071594c7d480253d3a7349eb136d
georgii <georg25@bk.ru>
2023-05-21 22:11:39 +0400

    Example005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Username - ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine ("Yey, its Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
